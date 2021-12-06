1effd39da7b717a7a4164836d6a37582639950a6
Logan Wagner <wagner17@stolaf.edu>
2021-12-05 22:00:42 -0600

    Forgot one file

--- App.js
+++ App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MainContainer from './library/MainContainer';
 
 import Login from './library/Login';
 import Inbox from './library/Inbox';
 import CreateAlert from './library/CreateAlert';
+import MainNavigator from './library/MainNavigtor';
 
 const Stack = createStackNavigator();
 
-
 export default function App() {
   return (
-    <MainContainer/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options = {{headerShown: false}}/>
+        <Stack.Screen name="Inbox" component={Inbox} options = {{headerShown: false}}/>
+        <Stack.Screen name = "Alert" component={CreateAlert} options = {{headerShown: false}} />
+        <Stack.Screen name = "Main Navigator" component = {MainNavigator} options = {{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
